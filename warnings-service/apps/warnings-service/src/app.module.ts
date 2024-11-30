4e7020c9156b4f458d54a4f90760b8f2bf174aa2
Kyrylo <kyrylo@lykho.com>
2024-11-30 17:37:13 +1100

    feat: bootstrap warnings-service

--- warnings-service/apps/warnings-service/src/app.module.ts
+++ warnings-service/apps/warnings-service/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { WarningsModule } from './warnings/warnings.module';
+import { SyncModule } from './sync/sync.module';
+import { DbModule } from './db/db.module';
+import { UtilsModule } from './utils/utils.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [WarningsModule, SyncModule, DbModule, UtilsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
