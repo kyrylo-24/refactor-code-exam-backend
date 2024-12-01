5441918c10b66064295cd9b6f4634da910c0f6b2
Kyrylo <kyrylo@lykho.com>
2024-12-01 17:05:52 +1100

    feat: update e2e tests to compare new and legacy apis

--- e2e/main.test.ts
+++ e2e/main.test.ts
@@ -1,48 +1,39 @@
 import { expect, test } from "bun:test";
 
-interface Data {
-    warningsByState: {
-        states: string[];
-        results: Record<string, string[]>;
-    };
-    detailedWarning: {
-        id: string;
-        result: any;
-    };
-}
-
-const data: Data = require("./data.json");
-
-const apiUrl = "http://localhost:3000";
+const legacyApiUrl = "http://localhost:3000";
+const newApiUrl = "http://localhost:4001";
 
 test("list warnings with states", async () => {
-    const states = data.warningsByState.states;
-    const results = data.warningsByState.results;
+    const legacyResponse = await fetch(`${legacyApiUrl}/?state=NSW`);
+    const newResponse = await fetch(`${newApiUrl}/?state=NSW`);
 
-    // Check each state has matching results
-    for (const state of states) {
-        console.log(`Checking state: ${state}`);
-        const response = await fetch(`${apiUrl}/?state=${state}`);
-        expect(response.status).toBe(200);
-
-        // Wait for server to update file
-        await new Promise(resolve => setTimeout(resolve, 1000));
-
-        const warnings = await response.json();
-        results[state].forEach(expectedWarning => {
-            expect(warnings).toContain(expectedWarning);
-        });
-    }
-});
+    expect(legacyResponse.status).toBe(200);
+    expect(newResponse.status).toBe(200);
+    expect(await legacyResponse.json()).toEqual(await newResponse.json());
+})
 
 test("list warnings without state", async () => {
-    const response = await fetch(`${apiUrl}/`);
-    expect(response.status).toBe(200);
-    expect(await response.json()).toEqual([]);
-});
+    const legacyResponse = await fetch(`${legacyApiUrl}/`);
+    const newResponse = await fetch(`${newApiUrl}/`);
 
-test("get detailed warning", async () => {
-    const response = await fetch(`${apiUrl}/warning/${data.detailedWarning.id}`);
-    expect(response.status).toBe(200);
-    expect(await response.json()).toEqual(data.detailedWarning.result);
-});
+    expect(legacyResponse.status).toBe(200);
+    expect(newResponse.status).toBe(200);
+    expect(await legacyResponse.json()).toEqual(await newResponse.json());
+})
+
+test("detailed warning", async () => {
+    const legacyResponse = await fetch(`${legacyApiUrl}/?state=NSW`);    
+    const legacyWarnings = await legacyResponse.json();
+    const legacyWarning = legacyWarnings[0];
+
+    const legacyWarningResponse = await fetch(`${legacyApiUrl}/warning/${legacyWarning}`);
+    const newWarningResponse = await fetch(`${newApiUrl}/warning/${legacyWarning}`);
+
+    expect(legacyWarningResponse.status).toBe(200);
+    expect(newWarningResponse.status).toBe(200);
+
+    const legacyWarningResult = await legacyWarningResponse.json();
+    const newWarningResult = await newWarningResponse.json();
+
+    expect(legacyWarningResult).toEqual(newWarningResult);
+})
